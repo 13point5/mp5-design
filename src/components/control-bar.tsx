f272e323da2081419c8432185259dbe3d874c5d8
Bharath Sriraam R R <albharath.1305@gmail.com>
2024-05-06 20:38:05 -0400

    fix error

--- src/components/control-bar.tsx
+++ src/components/control-bar.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect, useRef } from "react";
 const ControlBar = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
-  const intervalRef = useRef(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   const handlePlayPause = () => {
     if (progress >= 100) {
@@ -19,7 +19,7 @@ const ControlBar = () => {
       intervalRef.current = setInterval(() => {
         setProgress((prevProgress) => {
           if (prevProgress >= 100) {
-            clearInterval(intervalRef.current);
+            intervalRef.current && clearInterval(intervalRef.current);
             setIsPlaying(false);
             return 100;
           }
@@ -27,10 +27,12 @@ const ControlBar = () => {
         });
       }, 1000);
     } else {
-      clearInterval(intervalRef.current);
+      intervalRef.current && clearInterval(intervalRef.current);
     }
 
-    return () => clearInterval(intervalRef.current);
+    return () => {
+      intervalRef.current && clearInterval(intervalRef.current);
+    };
   }, [isPlaying]);
 
   return (
